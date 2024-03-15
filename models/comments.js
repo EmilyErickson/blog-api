ccc5f400d2e72435c9d00840de66291d108816cb
EmilyErickson <emilyerickson2002@gmail.com>
2024-03-15 11:26:30 -0500

    Create comment model

---
+++ models/comments.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
+  authorEmail: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+module.exports = Comment;
