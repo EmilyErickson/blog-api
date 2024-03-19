53f26087d297c92beac8963de51d4fd47236fe21
EmilyErickson <emilyerickson2002@gmail.com>
2024-03-19 14:14:29 -0500

    Require correct email syntax

--- models/comments.js
+++ models/comments.js
@@ -3,9 +3,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-  authorEmail: { type: String, required: true },
+  authorEmail: {
+    type: String,
+    required: true,
+    match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  },
   content: { type: String, required: true },
   timestamp: { type: Date, default: Date.now },
+  post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
 });
 
 const Comment = mongoose.model("Comment", commentSchema);
