c534cad66b367f7a2352f419c9566b6c1f938b52
EmilyErickson <emilyerickson2002@gmail.com>
2024-03-19 09:31:35 -0500

    Create post routes

---
+++ routes/post.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const postController = require("../controllers/postController");
+
+//Post Method "/api/post/create"
+router.post("/create", postController.post_create_post);
+
+//Get all Method "/api/post/all"
+router.get("/all", postController.get_all_posts);
+
+//Get by ID Method "/api/post/:id"
+router.get("/:id", postController.get_single_post);
+
+//Update by ID Method "/api/post/update/:id"
+router.put("/update/:id", postController.put_update_post);
+
+//Delete by ID Method "/api/post/delete/:id"
+router.delete("/delete/:id", postController.delete_single_post);
+
+module.exports = router;
