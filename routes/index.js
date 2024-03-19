779c9d5618f6ba4c61a441f93d0371a8dc27056b
EmilyErickson <emilyerickson2002@gmail.com>
2024-03-19 09:43:59 -0500

    Create index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+const postController = require("../controllers/postController");
+
+//Post Method
+router.get("/", postController.get_all_posts);
 
 module.exports = router;
