fe490a1082e227bcf45a89f20ed059afcbfc829d
EmilyErickson <emilyerickson2002@gmail.com>
2024-03-22 14:11:34 -0500

    Fix naming to match comment controller

--- models/comments.js
+++ models/comments.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-  authorEmail: {
+  email: {
     type: String,
     required: true,
     match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
   },
   content: { type: String, required: true },
   timestamp: { type: Date, default: Date.now },
-  post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
+  postId: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
 });
 
 const Comment = mongoose.model("Comment", commentSchema);
