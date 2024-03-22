2bb0ad405e079dd9bc84c74e86b1dcc206be0769
EmilyErickson <emilyerickson2002@gmail.com>
2024-03-22 14:01:39 -0500

    Implement passport local strategy and bcryptjs

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 require("dotenv").config();
 const createError = require("http-errors");
 const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const postRouter = require("./routes/post");
+const commentRouter = require("./routes/comments");
+const User = require("./models/users");
+
+const LocalStrategy = require("passport-local").Strategy;
 
 const app = express();
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 },
+  })
+);
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
@@ -21,18 +37,62 @@ async function main() {
   await mongoose.connect(mongoDB);
 }
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, {
+          message: "We can't find a user with that username",
+        });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/api", indexRouter);
+app.use("/api/user", usersRouter);
+app.use("/api/post", postRouter);
+app.use("/api/comment", commentRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
