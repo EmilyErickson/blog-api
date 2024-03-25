5d9e1c45e9e30379874203e9a79f3800a8c29d5e
EmilyErickson <emilyerickson2002@gmail.com>
2024-03-25 11:16:03 -0500

    Fix import of index veiw file

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path"); // Import the path module
+const index = path.join(__dirname, "../views/index.ejs"); // Correct path to index.ejs
 
 const postController = require("../controllers/postController");
 
 //Post Method
 router.get("/", (req, res) => {
-  res.render("view/index", { title: "Blog" });
+  res.render(index, { title: "Blog" });
 });
 module.exports = router;
